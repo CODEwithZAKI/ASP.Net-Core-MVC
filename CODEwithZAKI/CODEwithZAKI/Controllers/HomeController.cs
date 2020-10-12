01eac029eacad2e1c077e3eb673d982a0ea6eb8f
CODEwithZAKI <cumaraani1887@gmail.com>
2020-10-12 11:00:53 +0300

    ViewBag explained

--- CODEwithZAKI/CODEwithZAKI/Controllers/HomeController.cs
+++ CODEwithZAKI/CODEwithZAKI/Controllers/HomeController.cs
@@ -3,13 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Dynamic;
 namespace CODEwithZAKI.Controllers
 {
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
+            dynamic data = new ExpandoObject();
+            data.Id = 1;
+            data.name = "zakicade";
+            ViewBag.Data = data;
             return View();
         }
         public ViewResult AboutUs()
